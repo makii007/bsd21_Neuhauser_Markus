ac894ee3849d519286cba38f85b64dd021f382e0
Markus Neuhauser <s50814@edu.campus02.at>
2022-05-20 10:42:22 +0200

    added Logger to main

--- src/main/java/at/campus02/bsd/main.java
+++ src/main/java/at/campus02/bsd/main.java
@@ -1,8 +1,12 @@
 package at.campus02.bsd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class main {
     public static void main(String[] args) {
         Calculator testCalculator = new Calculator();
+        Logger logger = LogManager.getLogger();
 
         double testNum1 = 39.6;
         double testNum2 = 11.7;
@@ -12,5 +16,7 @@ public class main {
         System.out.println(testCalculator.divide(testNum1,testNum2));
         System.out.println(testCalculator.multiply(testNum1,testNum2));
 
+        logger.info("It is a info Logger");
+        logger.error("It is an error Logger");
     }
 }
